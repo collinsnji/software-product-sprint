09f1d1fa4efbdc7f1a2fbcaf81ef542883e89ba8
collinsnji <collins.geek@gmail.com>
2021-03-03 13:04:24 +0000

    Add a quote endpoint to the servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/QuoteGenerator.java
@@ -0,0 +1,18 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles requests sent to the /quote URL endpoint */
+@WebServlet("/quote")
+public class QuoteGenerator extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>\"Life changes fast. Life changes in the instant.\" - Joan Didion</h1>");
+  }
+}
