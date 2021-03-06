d8fe8d733e687381a5a4792e83cd0cad270a3823
collinsnji <collins.geek@gmail.com>
2021-03-06 22:00:08 +0000

    Add script to fetch quotes from server

---
+++ portfolio/src/main/webapp/quotes.js
@@ -0,0 +1,14 @@
+/**
+ * Fetch quotes from the server and appends them to the page
+ */
+const addQuotesToPage = async () => {
+    const serverResponse = await fetch('/quote');
+    const responseText = await serverResponse.text();
+
+    // Get the quote container and add the quote into it
+    const quoteContainer = document.querySelector('.quote-container');
+    quoteContainer.innerHTML = responseText;
+}
+
+// Execute the fetch once the DOM content is loaded and parse
+document.addEventListener("DOMContentLoaded", addQuotesToPage);
