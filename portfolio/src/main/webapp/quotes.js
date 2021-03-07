779cbee946905ac006de9c2fa6d6752b4c96c734
collinsji <collins.geek@gmail.com>
2021-03-07 02:43:31 +0000

    Parse quotes from JSON data and display random quote

--- portfolio/src/main/webapp/quotes.js
+++ portfolio/src/main/webapp/quotes.js
@@ -1,14 +1,21 @@
 /**
  * Fetch quotes from the server and appends them to the page
  */
-const addQuotesToPage = async () => {
-    const serverResponse = await fetch('/quote');
-    const responseText = await serverResponse.text();
+async function addQuotesToPage() {
+  const serverResponse = await fetch('/quote');
+  const responseJson = await serverResponse.json();
 
-    // Get the quote container and add the quote into it
-    const quoteContainer = document.querySelector('.quote-container');
-    quoteContainer.innerHTML = responseText;
-}
+  // Get a random quote from the json quotes
+  const quote =
+      responseJson
+          .quotes[Math.floor(Math.random() * responseJson.quotes.length)];
+  const quoteAuthor = quote.authorName;
+  const quoteText = quote.quoteText;
+
+  // Get the quote container and add the quote into it
+  const quoteContainer = document.querySelector('.quote-container');
+  quoteContainer.innerHTML = `"${quoteText}" - ${quoteAuthor}`;
+};
 
 // Execute the fetch once the DOM content is loaded and parse
-document.addEventListener("DOMContentLoaded", addQuotesToPage);
+document.addEventListener('DOMContentLoaded', addQuotesToPage);
