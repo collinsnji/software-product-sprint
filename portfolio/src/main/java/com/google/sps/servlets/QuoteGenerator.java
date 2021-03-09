d30743d20adfaa53765dcbbd6e0ce905bea3ccd1
collinsji <collins.geek@gmail.com>
2021-03-09 15:41:27 +0000

    Use the quote class instead of hashmaps and return only one random quote

--- portfolio/src/main/java/com/google/sps/servlets/QuoteGenerator.java
+++ portfolio/src/main/java/com/google/sps/servlets/QuoteGenerator.java
@@ -1,9 +1,10 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
+import com.google.sps.quote.Quote;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Random;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,38 +13,48 @@ import javax.servlet.http.HttpServletResponse;
 /** Handles requests sent to the /quote URL endpoint */
 @WebServlet("/quote")
 public class QuoteGenerator extends HttpServlet {
-  /**
-   * Build the quotes using the author name and quote text.
-   *
-   * @param authorName The author of the quote's name
-   * @param quoteText  The quote text
-   * @return A map with the author's name and the text
-   */
-  private HashMap<String, String> buildQuote(final String authorName, final String quoteText) {
-    final HashMap<String, String> quoteInfo = new HashMap<>();
-    quoteInfo.put("authorName", authorName);
-    quoteInfo.put("quoteText", quoteText);
-    return quoteInfo;
-  }
-
   @Override
   public void doGet(final HttpServletRequest request, final HttpServletResponse response)
       throws IOException {
+    // Get a random quote
+    Quote randomQuote = this.getRandomQuote();
+
+    // Parse the quote and return the json output
+    final Gson gson = new Gson();
+    final String json = gson.toJson(randomQuote);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  /**
+   * Build the quotes using the author name and quote text.
+   *
+   * @param authorName Name of the quote author
+   * @param quoteText  The quote text
+   * @return A Quote object with the author's name and the text
+   */
+  private Quote buildQuote(final String authorName, final String quoteText) {
+    final Quote quoteInfo = new Quote(authorName, quoteText);
+    return quoteInfo;
+  }
+
+  /**
+   * Return one random quote from an ArrayList of quotes
+   *
+   * @return a random quote
+   */
+  private Quote getRandomQuote() {
     // Add all to the quotes ArrayList
-    final ArrayList<HashMap<String, String>> quotes = new ArrayList<>();
+    final ArrayList<Quote> quotes = new ArrayList<>();
     quotes.add(buildQuote("Toni Morrision", "Correct what you can; learn from what you can\'t."));
     quotes.add(buildQuote("Joan Didion", "Life changes fast. Life changes in the in the instant."));
     quotes.add(buildQuote(
         "Maya Angelou", "I did then what I knew how to do. Now that I know better, I do better."));
 
-    // Create a HashMap to store all the quotes along with the authors
-    final HashMap<String, ArrayList<HashMap<String, String>>> quotesJson = new HashMap<>();
-    quotesJson.put("quotes", quotes);
-
-    // Parse the data and return the json output
-    final Gson gson = new Gson();
-    final String json = gson.toJson(quotesJson);
-    response.setContentType("application/json;");
-    response.getWriter().println(json);
+    // Return a quote from the ArrayList
+    Random random = new Random();
+    int randomIndex = random.nextInt(quotes.size());
+    Quote randomQuote = quotes.get(randomIndex);
+    return randomQuote;
   }
 }
