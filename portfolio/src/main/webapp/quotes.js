fdbd01299208bcf5c9bd9790cec25b3c11236d46
Collin Grimm <collins.geek@gmail.com>
2021-03-10 00:29:23 -0500

    Add week 2 step 3 work - using fetch with JSON data (#7)
    * Add gson dependency
    
    * Generate json output with quotes from various authors
    
    * Parse quotes from JSON data and display random quote
    
    * Correct typo in code comment
    
    * Fetch single quote from server instead of all quotes
    
    * Add a quote class to avoid using hashmaps
    
    * Use the quote class instead of hashmaps and return only one random quote
    
    * Use third person in comments

--- portfolio/src/main/webapp/quotes.js
+++ portfolio/src/main/webapp/quotes.js
@@ -1,14 +1,18 @@
 /**
  * Fetch quotes from the server and appends them to the page
  */
-const addQuotesToPage = async () => {
-    const serverResponse = await fetch('/quote');
-    const responseText = await serverResponse.text();
+async function addQuotesToPage() {
+  const serverResponse = await fetch('/quote');
+  const responseJson = await serverResponse.json();
 
-    // Get the quote container and add the quote into it
-    const quoteContainer = document.querySelector('.quote-container');
-    quoteContainer.innerHTML = responseText;
-}
+  // Get a random quote from the json quotes
+  const quoteAuthor = responseJson.authorName;
+  const quoteText = responseJson.quoteText;
+
+  // Get the quote container and add the quote into it
+  const quoteContainer = document.querySelector('.quote-container');
+  quoteContainer.innerHTML = `"${quoteText}" - ${quoteAuthor}`;
+};
 
 // Execute the fetch once the DOM content is loaded and parsed
-document.addEventListener("DOMContentLoaded", addQuotesToPage);
+document.addEventListener('DOMContentLoaded', addQuotesToPage);
